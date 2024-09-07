31bcbab25b0548583475640648ffd3efe98c8786
Aditya Shibu <as2397@hw.ac.uk>
2024-09-08 01:38:54 +0400

    Completed stat

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -137,11 +137,25 @@ const Stats = () => {
                     <h1 className="text-4xl font-bold text-accent text-center">Stats</h1>
                 </div>
                 {/* Stats Section */}
-                <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
+                <div className="flex flex-col xl:flex-row flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
                     {stats.map((stat, index) => (
-                        <div key={index} className="flex-1 flex gap-4 items-center xl:justify-start">
-                            <CountUp end={stat.num} duration={5} delay={1} className="text-4xl xl:text-6xl font-extrabold" />
-                            <p className={`${stat.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"} leading-snug text-white/80 text-center`}>{stat.text}</p>
+                        <div
+                            key={index}
+                            className="flex-1 grid grid-cols-2 gap-4 items-center w-full justify-center p-4 rounded-lg xl:bg-transparent"
+                        >
+                            <div className="flex justify-center items-center">
+                                <CountUp
+                                    end={stat.num}
+                                    duration={5}
+                                    delay={1}
+                                    className="text-5xl xl:text-7xl font-extrabold"
+                                />
+                            </div>
+                            <div className="flex justify-start items-center">
+                                <p className="leading-snug text-white/80 text-left">
+                                    {stat.text}
+                                </p>
+                            </div>
                         </div>
                     ))}
                 </div>
