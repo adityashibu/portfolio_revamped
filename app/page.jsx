ed8e1d232748a8e336c6e1b61268401bb24ed35d
Aditya Shibu <as2397@hw.ac.uk>
2024-09-07 13:58:46 +0400

    Added some basic page design

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,14 @@
-export default function Home() {
-  return <main>homepage</main>
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto">
+        home page
+      </div>
+    </section>
+  )
 }
+
+export default Home
