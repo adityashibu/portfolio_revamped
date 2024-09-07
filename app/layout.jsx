6d6328793577575e9c6d782ac573b00e25fb5b26
Aditya Shibu <as2397@hw.ac.uk>
2024-09-07 10:13:07 +0400

    Added base code for the header

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,12 @@
-import localFont from "next/font/local";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+// Import components
+import Header from "@/components/Header";
+
+const jetBrainsMono = JetBrains_Mono({
+  subsets: ["latin"], weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: '--font-jetbrainsMono'
 });
 
 export const metadata = {
@@ -21,8 +18,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${jetBrainsMono.variable}`}
       >
+        <Header />
         {children}
       </body>
     </html>
