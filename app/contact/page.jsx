c8f8cd8ff29cbaaebac84562ab214640240a6f74
Aditya Shibu <as2397@hw.ac.uk>
2024-09-11 23:25:16 +0400

    Escaped ' with it's ascii wtv

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -48,7 +48,7 @@ const Contact = () => {
           {/* Form */}
           <div className="xl:h-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-accent">Let's get in touch</h3>
+              <h3 className="text-4xl text-accent">Let&#39;s get in touch</h3>
               <p className="text-white/60 ">
                 Fill in the below form or contact me directly using any of the
                 provided information, I would love to get in touch
